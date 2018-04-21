a714a17ed57c22b04a29181b6e7d1d6c9f47b196
tayfun <tayfun@marion.cs.hacettepe.edu.tr>
2018-04-21 13:16:25 +0300

    setup.py is changed according to version of pip installed.

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ The setup script for this project.
 @author: Christoph Lassner
 """
 from setuptools import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 VERSION = '0.1'
 REQS = [str(ir.req) for ir in parse_requirements('requirements.txt',
